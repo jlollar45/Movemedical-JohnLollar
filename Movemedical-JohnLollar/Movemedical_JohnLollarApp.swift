d92cf485d54e122df6aa92f4d1b6f5ffb714ca30
John Lollar <jlollar45@gmail.com>
2022-11-04 22:16:05 -0500

    Initial Commit

---
+++ Movemedical-JohnLollar/Movemedical_JohnLollarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movemedical_JohnLollarApp.swift
+//  Movemedical-JohnLollar
+//
+//  Created by John Lollar on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Movemedical_JohnLollarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
